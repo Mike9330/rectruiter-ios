ca09b7b6dc5b6bd677689f8dbda31e56e81764c9
michaeldonato <mmd1130@gmail.com>
2025-05-27 13:18:02 -0500

    Adds feed tab

--- recruiter-ios/ContentView.swift
+++ recruiter-ios/ContentView.swift
@@ -3,6 +3,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
+            Text("Feed Coming Soon")
+                .tabItem {
+                    Label("Feed", systemImage: "newspaper.fill")
+                }
+            
             ReviewView()
                 .tabItem {
                     Label("Review", systemImage: "pencil.circle.fill")
